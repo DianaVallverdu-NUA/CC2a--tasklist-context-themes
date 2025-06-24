b8e62a78c6feb451e51fd5a44e1d05b45caaa017
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-06-24 16:07:52 +0100

    fix input issues

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,6 +1,8 @@
 import { TaskListContext } from "@/contexts/TaskListContext";
-import { useContext, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Checkbox } from '@futurejj/react-native-checkbox';
+import Icon from "@react-native-vector-icons/fontawesome6";
+import React, { useContext, useState } from "react";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 
 interface TaskProps {
   description: string;
@@ -29,7 +31,10 @@ const Task = (props: TaskProps) => {
     <View style={styles.container}>
       <View style={styles.leftArea}>
         {/* checkbox input */}
-        <input type="checkbox" onChange={onCheckboxChange}></input>
+        <Checkbox
+        status={checked ? 'checked' : 'unchecked'}
+        onPress={onCheckboxChange}
+      />
 
         {/* display task description */}
         <Text
@@ -41,13 +46,14 @@ const Task = (props: TaskProps) => {
           {description}
         </Text>
       </View>
-
       {/* button to delete task from main tasklist */}
-      <Button
-        color="red"
-        title="Delete Task"
-        onPress={onDeleteHandler}
-      ></Button>
+      <TouchableHighlight onPress={onDeleteHandler}>
+        <View>
+          <Text>
+            <Icon name="trash" size={20} color="" iconStyle="solid" />
+          </Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 };
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,16 +1,23 @@
+import { Text } from "@react-navigation/elements";
+import { useEffect, useState } from "react";
+
 // interfaces & props
 interface TitleProps {
-  myName?: string
+  myName?: string;
 }
 
 // main class
 const Title = (props: TitleProps) => {
+  const [title, setTitle] = useState("My To Do List");
 
   // use ternary operation to decide tasklist Title if name is / isn't defined
-  const titleToShow = props.myName ? `${props.myName}'s To Do List` : "My To Do List";
+  useEffect(() => {
+    if (props.myName) setTitle(`${props.myName}'s To Do List`);
+    else setTitle("My To Do List");
+  }, [props.myName]);
 
   // display title
-  return (<h1>{titleToShow}!</h1>)
-}
+  return <Text>{title}!</Text>;
+};
 
 export default Title;
