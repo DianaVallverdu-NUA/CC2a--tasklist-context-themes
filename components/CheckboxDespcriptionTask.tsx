43a0652ded3b085268acdc7cbbe2ff5f50656f9d
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-06-24 16:38:50 +0100

    checkbox & description as sub component

---
+++ components/CheckboxDespcriptionTask.tsx
@@ -0,0 +1,47 @@
+import { TaskContext } from "@/contexts/TaskContext";
+import { Checkbox } from "@futurejj/react-native-checkbox";
+import { useContext, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CheckboxDescriptionTask = () => {
+  // extract props
+  const { description } = useContext(TaskContext);
+
+  // state - manage when checkbox is checked
+  const [checked, setChecked] = useState<Boolean>(false);
+
+  // handlers
+  const onCheckboxChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* checkbox input */}
+      <Checkbox
+        status={checked ? "checked" : "unchecked"}
+        onPress={onCheckboxChange}
+      />
+
+      {/* display task description */}
+      <Text
+        style={{
+          ...styles.descriptionText,
+          textDecorationLine: checked ? "line-through" : "none",
+        }}
+      >
+        {description}
+      </Text>
+    </View>
+  );
+};
+
+// styles
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  descriptionText: { marginTop: 7 },
+});
+
+export default CheckboxDescriptionTask;
--- components/Task.tsx
+++ components/Task.tsx
@@ -1,50 +1,16 @@
-import { TaskContext } from "@/contexts/TaskContext";
-import { TaskListContext } from "@/contexts/TaskListContext";
-import { Checkbox } from "@futurejj/react-native-checkbox";
-import React, { useContext, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import CheckboxDescriptionTask from "./CheckboxDespcriptionTask";
 import EditDeleteTask from "./EditDeleteTask";
 
 const Task = () => {
-  // extract props
-  const { description, id } = useContext(TaskContext);
-
-  const { deleteTask } = useContext(TaskListContext);
-
-  // state - manage when checkbox is checked
-  const [checked, setChecked] = useState<Boolean>(false);
-
-  // handlers
-  const onCheckboxChange = () => {
-    setChecked(!checked);
-  };
-
-  const onDeleteHandler = () => {
-    deleteTask(id);
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.leftSection}>
-        {/* checkbox input */}
-        <Checkbox
-          status={checked ? "checked" : "unchecked"}
-          onPress={onCheckboxChange}
-        />
-
-        {/* display task description */}
-        <Text
-          style={{
-            ...styles.descriptionText,
-            textDecorationLine: checked ? "line-through" : "none",
-          }}
-        >
-          {description}
-        </Text>
-      </View>
+      {/* checkbox & description */}
+      <CheckboxDescriptionTask />
 
       {/* button to edit task from main tasklist */}
-        <EditDeleteTask />
+      <EditDeleteTask />
     </View>
   );
 };
@@ -57,9 +23,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     width: 300,
     marginBottom: 4,
-  },
-  leftSection: {
-    flexDirection: "row",
   },
   descriptionText: { marginTop: 7 },
 });
